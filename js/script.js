c5773d251614409ea034e422220904e33e0e369e
Filip <example@gmail.com>
2023-01-31 23:50:03 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// // Варіант 8
+
+
+function f(x) {
+    return (20 / (1 + (x * x))).toFixed(2);
+
+}
+
+console.log(f(25), f(16), f(3), f(2), f(8));
+
+
+
+
+let a = 4, b = 6, n = 40;
+
+for (let i = a; i <= b; i += n) {
+    (i);
+}
+

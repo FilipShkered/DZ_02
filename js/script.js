fc789d1bcf79f36a3090916b9a0adc6dd02d9fbf
Filip <example@gmail.com>
2023-02-01 03:14:25 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
 // // Варіант 8
 
 
-function f(x) {
-    return (20 / (1 + (x * x))).toFixed(2);
-
-}
-
-console.log(f(25), f(16), f(3), f(2), f(8));
-
-
-
-
 let a = 4, b = 6, n = 40;
+let h = (b - a) / n;
 
-for (let i = a; i <= b; i += n) {
-    (i);
+// функція 25
+for (let x = a; x <= b; x += h) {
+  let y25 = x + Math.sin(x) - Math.cos(x*3-1);
+  console.log(x, y25);
 }
+
+// функція 16
+for (let x = a; x <= b; x += h) {
+  let y16 = 2*(-x) / 100;
+  console.log(x, y16);
+}
+
+// функція 3
+for (let x = a; x <= b; x += h) {
+  let y3 = Math.pow(x + 4, 5);
+  console.log(x, y3);
+}
+
+// функція 2
+for (let x = a; x <= b; x += h) {
+  let y2 = Math.exp(x);
+  console.log(x, y2);
+}
+
+// функція 8
+for (let x = a; x <= b; x += h) {
+  let y8 = 20 / (1 + x * x);
+  console.log(x, y8);
+}
+
+
 
